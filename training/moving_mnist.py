9e157df135f33a3e5d28e0761ac0460a27f3f417
sam <sam@thinky.ai>
2024-07-10 20:16:08 +0000

    Bump the default save interval.

--- training/moving_mnist.py
+++ training/moving_mnist.py
@@ -418,7 +418,7 @@ def main(override=None):
     parser.add_argument("--num_training_steps", type=int, default=10000)
     parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--config_path", type=str, default="configs/glide.yaml")
-    parser.add_argument("--save_and_sample_every_n", type=int, default=100)
+    parser.add_argument("--save_and_sample_every_n", type=int, default=1000)
     parser.add_argument("--load_model_weights_from_checkpoint", type=str, default="")
     parser.add_argument("--resume_from", type=str, default="")
     args = parser.parse_args()
